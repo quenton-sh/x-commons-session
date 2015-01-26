149dd71c666e885006a0fdc3a530d586a71bddfb
Quenton Shang <quenton2010@gmail.com>
2015-01-26 12:44:56 +0800

    支持针对不同类别设定不同的会话有效期

--- src/main/java/x/commons/session/impl/DefaultSessionManager.java
+++ src/main/java/x/commons/session/impl/DefaultSessionManager.java
@@ -1,18 +1,27 @@
 package x.commons.session.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import x.commons.session.Session;
 import x.commons.session.SessionIDGenerator;
 import x.commons.session.SessionManager;
 import x.commons.session.SessionStore;
 
-public class DefaultSessionManager<T extends Session> implements SessionManager<T> {
+public class DefaultSessionManager<T extends Session> implements
+		SessionManager<T> {
 
-	private int expireSecs = 900; // 默认会话有效期：15分钟
+	private int defaultSessionTimeout = 900; // 默认会话有效期：15分钟
+	private Map<Integer, Integer> sessionTimeout = new HashMap<Integer, Integer>();
 	private SessionIDGenerator sessionIDGenerator;
 	private SessionStore<T> sessionStore;
 
-	public void setExpireSecs(int expireSecs) {
-		this.expireSecs = expireSecs;
+	public void setDefaultSessionTimeout(int defaultSessionTimeout) {
+		this.defaultSessionTimeout = defaultSessionTimeout;
+	}
+
+	public void setSessionTimeout(Map<Integer, Integer> sessionTimeout) {
+		this.sessionTimeout = sessionTimeout;
 	}
 
 	public void setSessionIDGenerator(SessionIDGenerator sessionIDGenerator) {
@@ -24,40 +33,35 @@ public class DefaultSessionManager<T extends Session> implements SessionManager<
 	}
 
 	@Override
-	public String createSession(T sessionObj) throws Exception {
-		String sessionID = this.sessionIDGenerator.generate();
-		sessionObj.setId(sessionID);
-		this.sessionStore.put(sessionObj, expireSecs);
-		return sessionID;
+	public String createSession(T sessionObj, int type) throws Exception {
+		return this.doCreateSession(sessionObj,
+				this.getSessionTimeoutForType(type));
 	}
-	
+
+	@Override
+	public boolean updateSession(T sessionObj, int type) throws Exception {
+		return this.doUpdateSession(sessionObj,
+				this.getSessionTimeoutForType(type));
+	}
+
+	@Override
+	public T validateSession(String sid, int type) throws Exception {
+		return this.doValidateSession(sid, this.getSessionTimeoutForType(type));
+	}
+
+	@Override
+	public String createSession(T sessionObj) throws Exception {
+		return this.doCreateSession(sessionObj, this.defaultSessionTimeout);
+	}
+
 	@Override
 	public boolean updateSession(T sessionObj) throws Exception {
-		if (sessionObj == null) {
-			return false;
-		}
-		if (this.validateSidAndGetSessionObj(sessionObj.getId()) == null) {
-			return false;
-		}
-		this.sessionStore.put(sessionObj, expireSecs);
-		return true;
+		return this.doUpdateSession(sessionObj, this.defaultSessionTimeout);
 	}
 
 	@Override
 	public T validateSession(String sid) throws Exception {
-		T obj = this.validateSidAndGetSessionObj(sid);
-		if (obj != null) {
-			// 刷新过期时间
-			this.sessionStore.put(obj, expireSecs);
-		}
-		return obj;
-	}
-	
-	private T validateSidAndGetSessionObj(String sid) throws Exception {
-		if (sid == null || !this.sessionIDGenerator.verify(sid)) {
-			return null;
-		}
-		return this.sessionStore.get(sid);
+		return this.doValidateSession(sid, this.defaultSessionTimeout);
 	}
 
 	@Override
@@ -66,6 +70,49 @@ public class DefaultSessionManager<T extends Session> implements SessionManager<
 			return null;
 		}
 		return this.sessionStore.remove(sid);
+	}
+
+	private T validateSidAndGetSessionObj(String sid) throws Exception {
+		if (sid == null || !this.sessionIDGenerator.verify(sid)) {
+			return null;
+		}
+		return this.sessionStore.get(sid);
+	}
+
+	private int getSessionTimeoutForType(int type) {
+		Integer sessionTimeout = this.sessionTimeout.get(type);
+		if (sessionTimeout != null) {
+			return sessionTimeout.intValue();
+		} else {
+			return this.defaultSessionTimeout;
+		}
+	}
+
+	private String doCreateSession(T sessionObj, int timeout) throws Exception {
+		String sessionID = this.sessionIDGenerator.generate();
+		sessionObj.setId(sessionID);
+		this.sessionStore.put(sessionObj, timeout);
+		return sessionID;
+	}
+
+	private boolean doUpdateSession(T sessionObj, int timeout) throws Exception {
+		if (sessionObj == null) {
+			return false;
+		}
+		if (this.validateSidAndGetSessionObj(sessionObj.getId()) == null) {
+			return false;
+		}
+		this.sessionStore.put(sessionObj, timeout);
+		return true;
+	}
+
+	private T doValidateSession(String sid, int timeout) throws Exception {
+		T obj = this.validateSidAndGetSessionObj(sid);
+		if (obj != null) {
+			// 刷新过期时间
+			this.sessionStore.put(obj, timeout);
+		}
+		return obj;
 	}
 
 }
--- src/test/java/x/commons/session/impl/InVMSessionManagerTest.java
+++ src/test/java/x/commons/session/impl/InVMSessionManagerTest.java
@@ -3,53 +3,91 @@ package x.commons.session.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import x.commons.session.Session;
 import x.commons.session.SessionManager;
-import x.commons.session.impl.InVMSessionManagerFactory;
 
 public class InVMSessionManagerTest {
+	
+	private static InVMSessionManagerFactory<Session> factory = null;
+	private static final int defaultType = 12345;
+	private static final int defaultSessionTimeout = 3;
+	
+	private static final int type1 = 1;
+	private static final int sessionTimeout1 = 1;
+
+	private static final int type2 = 2;
+	private static final int sessionTimeout2 = 2;
+	
+	@BeforeClass
+	public static void init() {
+		factory = new InVMSessionManagerFactory<Session>();
+		factory.setDefaultSessionTimeout(defaultSessionTimeout);
+		
+		Map<Integer, Integer> timeouts = new HashMap<Integer, Integer>();
+		timeouts.put(type1, sessionTimeout1);
+		timeouts.put(type2, sessionTimeout2);
+		factory.setSessionTimeout(timeouts);
+	}
 
 	@Test
-	public void test() throws Exception {
-		final int expireSecs = 2; // 会话有效期：2秒
+	public void testType1() throws Exception {
+		toTestForType(type1, sessionTimeout1);
+	}
+	
+	@Test
+	public void testType2() throws Exception {
+		toTestForType(type2, sessionTimeout2);
+	}
+	
+	@Test
+	public void testDefault() throws Exception {
+		toTestForType(defaultType, defaultSessionTimeout);
+	}
+	
+	private void toTestForType(int type, int sessionTimeout) throws Exception {
 		Session session = new Session();
-		InVMSessionManagerFactory<Session> factory = new InVMSessionManagerFactory<Session>();
-		factory.setExpireSecs(expireSecs);
 		SessionManager<Session> sm = factory.getSessionManager();
 		
+		sessionTimeout = sessionTimeout * 1000; // s --> ms
+		
 		// 建立session
-		String sid1 = sm.createSession(session);
-		Session obj = sm.validateSession(sid1);
+		String sid1 = sm.createSession(session, type);
+		Session obj = sm.validateSession(sid1, type);
 		assertTrue(obj != null);
 		assertEquals(session.getId(), obj.getId());
 		
 		// 验证后刷新过期时间
-		Thread.sleep(1800);
-		obj = sm.validateSession(sid1);
+		Thread.sleep(sessionTimeout - 100);
+		obj = sm.validateSession(sid1, type);
 		assertTrue(obj != null);
-		Thread.sleep(1800);
-		obj = sm.validateSession(sid1);
+		
+		Thread.sleep(sessionTimeout - 100);
+		obj = sm.validateSession(sid1, type);
 		assertTrue(obj != null);
 		
 		// session过期
-		Thread.sleep(2000);
-		obj = sm.validateSession(sid1);
+		Thread.sleep(sessionTimeout);
+		obj = sm.validateSession(sid1, type);
 		assertTrue(obj == null);
 		
 		// 重新建立session
-		String sid2 = sm.createSession(session);
+		String sid2 = sm.createSession(session, type);
 		assertTrue(sid2 != null);
 		assertTrue(!sid2.equals(sid1));
-		obj = sm.validateSession(sid2);
+		obj = sm.validateSession(sid2, type);
 		assertTrue(obj != null);
 		assertEquals(session.getId(), obj.getId());
 		
 		// 删除session
 		Session removedSessionObj = sm.destroySession(sid2);
 		assertTrue(removedSessionObj != null && removedSessionObj.getId().equals(session.getId()));
-		obj = sm.validateSession(sid2);
+		obj = sm.validateSession(sid2, type);
 		assertTrue(obj == null);
 	}
 }
